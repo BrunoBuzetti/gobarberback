bd8b7ded3a12212c1a97140215251fa16a1b0c19
BrunoBuzetti <70170336+BrunoBuzetti@users.noreply.github.com>
2020-11-03 22:14:05 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { request, response } from 'express';
+import routes from './routes/index';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!')
+});
